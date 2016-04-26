d62b1e8e5c97420e6ccedb6071bd2c3ab7b7cfed
endoze <endoze@endozemedia.com>
2016-04-26 17:43:01 -0400

    Remove UIKit import from module header
    In order to avoid pulling in unnecessary frameworks (UIKit), this commit
    replaces the import with one pulling in Foundation instead.

--- Initializable.podspec
+++ Initializable.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = "Initializable"
-  s.version      = "1.0.0"
+  s.version      = "1.0.1"
   s.summary      = "Application Initializer protocols"
 
   s.description  = <<-DESC
--- Initializable/Initializable.h
+++ Initializable/Initializable.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 Wide Eye Labs. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Initializable.
 FOUNDATION_EXPORT double InitializableVersionNumber;
