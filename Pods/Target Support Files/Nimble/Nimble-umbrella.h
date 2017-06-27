6029c66d98a361b4bc4fe739b629077bc1108439
Chris <chris@wideeyelabs.com>
2017-06-27 17:17:25 -0400

    Update cocoapods
    This commit updates the project's cocoapod dependencies to newer
    versions.

--- Pods/Target Support Files/Nimble/Nimble-umbrella.h
+++ Pods/Target Support Files/Nimble/Nimble-umbrella.h
@@ -1,9 +1,13 @@
 #import <Cocoa/Cocoa.h>
 
+#import "CwlCatchException.h"
+#import "CwlMachBadInstructionHandler.h"
+#import "mach_excServer.h"
+#import "CwlPreconditionTesting.h"
+#import "Nimble.h"
 #import "DSL.h"
 #import "NMBExceptionCapture.h"
 #import "NMBStringify.h"
-#import "Nimble.h"
 
 FOUNDATION_EXPORT double NimbleVersionNumber;
 FOUNDATION_EXPORT const unsigned char NimbleVersionString[];
